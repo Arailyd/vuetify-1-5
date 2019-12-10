3c2d60643d434392ee40ac328bb2ba1d92dee7a5
Arailyd <avrcorldr@gmail.com>
2019-12-09 22:59:19 -0400

    Commit Origen de practica

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,6 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere eum
-      aliquam quibusdam dicta ipsa nemo saepe, ducimus, nulla error ea nostrum
-      dignissimos, iusto doloribus? Harum rem sint alias consectetur dicta.
-    </p>
-    <p class="pink lighten-4 red--text text--darken-4">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-      repudiandae, optio eius numquam dolor amet excepturi hic odio, sunt
-      voluptates voluptate nobis soluta quae maiores quaerat, dignissimos porro
-      fuga laboriosam.
-    </p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller display</h4>
-    <p class="headline">This is a headline</p>
-    <p class="subtitle-1 font-weight-bold">This is a subheading</p>
-    <p class="caption">This is a caption</p>
   </div>
 </template>
 
