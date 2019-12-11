7d4d61ad2a4f88ea7ac0fe3ef9cb5be39d1b3296
Arailyd <avrcorldr@gmail.com>
2019-12-11 18:42:43 -0400

    Segundo commit del proyecto vuetify en el video 10, arreglados los problemas de iconos y rutas en la lista

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
     <v-toolbar flat app>
-      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        class="grey--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
@@ -14,17 +17,16 @@
     </v-toolbar>
 
     <v-navigation-drawer v-model="drawer" absolute temporary class="primary">
-      <v-list>
-        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
-          <v-list-tile-action>
-            <v-icon class="white--text">mdi-{{link.icon}}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title class="white--text">
-              {{link.text}}
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+      <v-list dense>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-icon>
+            <v-icon color="white">{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content class="white--text">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
   </nav>
@@ -32,15 +34,21 @@
 
 <script>
 export default {
-  data(){
-    return{
+  data() {
+    return {
       drawer: false,
-      links:[
-        {icon: 'dashboard', text: 'Dashboard', route:'/'},
-        {icon: 'folder', text: 'My Projects', route:'/projects'},
-        {icon: 'person', text: 'Team', route:'/team'},
+      links: [
+        { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-account", text: "Team", route: "/team" }
       ]
-    }
+      // return {
+      //     drawer: null,
+      //     items: [
+      //       { title: 'Home', icon: 'mdi-view-dashboard' },
+      //       { title: 'About', icon: 'mdi-forum' },
+      //     ],
+    };
   }
 };
 </script>
