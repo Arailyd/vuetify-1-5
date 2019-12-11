64ed023e71dcb0eb231ff0539726caff43afb744
Arailyd <avrcorldr@gmail.com>
2019-12-10 23:35:32 -0400

    Primer commit del proyecto vuetify en el video 10 hay que arreglar las listas

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,18 @@
     </v-toolbar>
 
     <v-navigation-drawer v-model="drawer" absolute temporary class="primary">
-      <p class="success">test</p>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">mdi-{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">
+              {{link.text}}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -23,7 +34,12 @@
 export default {
   data(){
     return{
-      drawer: false
+      drawer: false,
+      links:[
+        {icon: 'dashboard', text: 'Dashboard', route:'/'},
+        {icon: 'folder', text: 'My Projects', route:'/projects'},
+        {icon: 'person', text: 'Team', route:'/team'},
+      ]
     }
   }
 };
