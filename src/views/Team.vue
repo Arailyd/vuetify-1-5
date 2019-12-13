c65cf357162088c2f6e8f437193db13e83682e8a
Arailyd <avrcorldr@gmail.com>
2019-12-13 00:46:08 -0400

    Commit de video 12, lo voy a probar despues

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="team">
-    <h1>Team</h1>
-
-
+    <h1 class="subheading grey--text">Team</h1>
+    <v-container  class="my-5">
+      <!-- <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo tempora
+        voluptatem voluptas ducimus animi adipisci, debitis eligendi dicta
+        pariatur eaque, eveniet ad laboriosam dolores nisi magnam distinctio
+        autem magni! Adipisci. Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Ullam sed dolore voluptatem nostrum eos ex aut
+        corporis quidem, inventore eligendi minus nobis expedita illum? Cumque
+        illo accusantium id dolore deleniti.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magnam
+        cumque facere tempora doloribus perspiciatis ullam animi iure eveniet
+        unde blanditiis veniam, a mollitia, quisquam molestias? Libero
+        consequatur pariatur fugit. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Beatae quam debitis architecto esse aut nam maxime
+        libero, voluptas laborum, accusamus atque nostrum, recusandae
+        perspiciatis inventore impedit ex? Exercitationem, dolore nemo.
+      </p> -->
+      content
+    </v-container>
   </div>
 </template>
 
